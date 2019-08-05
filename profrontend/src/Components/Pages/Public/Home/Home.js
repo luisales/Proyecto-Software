794d173f4f25d5b455cc7bd45c5109a8ed30c119
Omar <floresaleman1998@hotmail.com>
2019-08-04 22:33:30 -0600

    scroll

--- profrontend/src/Components/Pages/Public/Home/Home.js
+++ profrontend/src/Components/Pages/Public/Home/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 import Button from '../../../Common/Btns/Buttons';
 import './Home.css';
 import { IoIosLogOut } from 'react-icons/io';
@@ -8,12 +9,12 @@ export default class Home extends Component{
     return (
       
         <div className="home">
-            
-           
+          <InfiniteScroll>  
             <h1>Pollos Vimart</h1>
             <img src="https://i.imgur.com/8kU7Mt2.jpg" />
             <div>&nbsp;</div>
             {(this.props.auth.logged) ? (<div className="half"><Button customClass="primary" onClick={(e) => { this.props.setUnAuth(false)}}><IoIosLogOut/>&nbsp;Logout</Button></div>):null}
+            </InfiniteScroll>
         </div>
     );
   }
