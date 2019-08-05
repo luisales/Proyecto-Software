29a23669ebd3db80b36f7b0a07df31b6a22ef911
Omar <floresaleman1998@hotmail.com>
2019-08-04 23:37:01 -0600

    original

--- profrontend/src/Components/Pages/Public/Home/Home.js
+++ profrontend/src/Components/Pages/Public/Home/Home.js
@@ -8,14 +8,12 @@ export default class Home extends Component{
   render() {
     return (
       
-        <div className="home">
-          <InfiniteScroll>  
+        <div className="home" >
             <h1>Pollos Vimart</h1>
             <img src="https://i.imgur.com/8kU7Mt2.jpg" />
             <div>&nbsp;</div>
             {(this.props.auth.logged) ? (<div className="half"><Button customClass="primary" onClick={(e) => { this.props.setUnAuth(false)}}><IoIosLogOut/>&nbsp;Logout</Button></div>):null}
-            </InfiniteScroll>
-        </div>
+          </div>
     );
   }
 
